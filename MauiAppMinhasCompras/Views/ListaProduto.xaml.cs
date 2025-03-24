a53903e723e1ad7223b25d57be55bc08b93e203a
mariagabers <mariagabers@gmail.com>
2025-03-24 02:52:11 -0300

    Finalização do básico do aplicativo - remover e selecionar itens da lista

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -98,4 +98,24 @@ public partial class ListaProduto : ContentPage
             await DisplayAlert("Ops!", ex.Message, "OK");
         }
     }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        try
+        {
+            Produto p = e.SelectedItem as Produto;
+
+            Navigation.PushAsync(new Views.EditarProduto
+            {
+                BindingContext = p,
+            });
+        }
+
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops!", ex.Message, "OK");
+
+        }
+    }
+    
 }
