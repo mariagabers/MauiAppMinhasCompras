673b36d2157420c6ecbc2eb01f0fabe6f5162395
mariagabers <mariagabers@gmail.com>
2025-03-24 03:18:56 -0300

    Adicionada validação para os campos Descrição, Quantidade e Preço na models Produto, Correção de produtos duplicados na tela inicial

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,11 +4,53 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        private string _descricao;
+        private double _quantidade;
+        private double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco {  get; set; }
-        public double Total { get => Quantidade * Preco; }
+
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null || value.Trim().Length == 0)
+                {
+                    throw new Exception("Por favor, preencha a descrição.");
+                }
+                _descricao = value;
+            }
+        }
+
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("A quantidade deve ser maior que zero.");
+                }
+                _quantidade = value;
+            }
+        }
+
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("O preço deve ser maior que zero.");
+                }
+                _preco = value;
+            }
+        }
+
+        public double Total => Quantidade * Preco;
     }
 }
+
